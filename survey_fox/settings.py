294b9e804e44d18f726ab7711095995d8cf282d7
Tahmidur Rahman <tahmidur@w3engineers.com>
2020-06-07 20:45:04 +0600

    Update settings

--- survey_fox/settings.py
+++ survey_fox/settings.py
@@ -47,12 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'survey',
     'auth_app',
-    'debug_toolbar'
+    #'debug_toolbar'
 ]
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
